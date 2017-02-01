e065be78ea219c8507bab951f12a292795eaea4e
Locky <locky.stewart@gmail.com>
2017-02-01 16:22:14 -0500

    hellow world

---
+++ validator.rb
@@ -0,0 +1,4 @@
+class Validator
+  def initialize()
+  end
+end
---
+++ validator_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './validator.rb'
+
+class ValidatorTest < Minitest::Test
+  def test_truth
+    assert true
+  end
+end
