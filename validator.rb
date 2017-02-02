ad1ba60b52338eef32f23ff0787944be0fef70cc
Locky <locky.stewart@gmail.com>
2017-02-02 13:39:24 -0500

    got ages working

--- validator.rb
+++ validator.rb
@@ -8,24 +8,37 @@ class Validator
 
   def baddates
     #creates a array of join dates
-    gooddates=[]
-    baddates=[]
-    row_one = @data.map{|person| person[1]}
-    dates = row_one.drop(1)
+    gooddates = []
+    baddates = []
+    column_one = @data.map{|person| person[1]}
+    dates = column_one.drop(1)
 
     dates.each do |string|
       gooddates += string.scan(/^\A[1-12][\/-][1-31][\/-]\d{2,4}\z$/)
       #how do I do 2 or 4, without the matchin groups issue?
     end
     baddates = dates - gooddates
-    baddates
+    baddates_location = baddates.map{|date| column_one.index(date)}
+    baddates_location
   end
-end
 
-def baddates_count
-  baddates.count
-end
+  def baddates_count
+    baddates.count
+  end
 
-def baddates_location
-  baddates_location = baddates.map{|date| row_one.index(date)}
+  def badages
+    goodages = []
+    badages = []
+    column_two = @data.map{|person| person[2]}
+    ages = column_two.drop(1)
+
+    ages.each do |string|
+      goodages += string.scan(/^\d{1,115}$/)
+    end
+    badages = ages - goodages
+    badages_location = badages.map{|date| column_two.index(date)}
+    badages_location
+
+  end
+
 end
--- validator_test.rb
+++ validator_test.rb
@@ -9,7 +9,7 @@ class ValidatorTest < Minitest::Test
 
   def test_baddates
     userinfo = Validator.new('userinformation.csv')
-    assert userinfo.baddates == ["2016-02", "13/03/2016", "442016", "2016-07-07", "Yesterday"]
+    assert userinfo.baddates == [2, 4, 5, 6, 7]
     #but what do I put in this array? Do i have to manually go check the file? do i put in the incoorect ones or the right ones?
   end
 
@@ -18,8 +18,9 @@ class ValidatorTest < Minitest::Test
     assert userinfo.baddates_count == 5
   end
 
-  def test_baddates_location
+  def test_badages
     userinfo = Validator.new('userinformation.csv')
-    assert userinfo.baddates_location == [2, 4, 5, 6, 7]
+    assert userinfo.badages == []
   end
+  
 end
